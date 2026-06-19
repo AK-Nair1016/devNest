74ce995e48c88c6612d13a25f0e4c07eb4e9ce3e
Aman K <aman.kuttannair1016@gmail.com>
2026-06-19 12:48:45 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express(); // creating instance of express 
+
+app.use((req, res)=> {
+    res.send("Hello  from the server!!");
+});
+app.listen(3000,()=> {
+    console.log("Server is successfully listening on port 3000")
+});

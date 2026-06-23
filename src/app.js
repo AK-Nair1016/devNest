271dd214dd0c023e943e7aa596980ad6521e0a99
Aman K <aman.kuttannair1016@gmail.com>
2026-06-24 00:45:08 +0530

    multiple route handling

--- src/app.js
+++ src/app.js
@@ -3,21 +3,27 @@ const express = require('express');
 const app = express(); // creating instance of express 
 
 //This will only handle GET call
-app.get("/user", (req,res)=>{
-    res.send({firstName:"Aman",lastName:"K"})
-});
-//this will only handle the POST call
-app.post("/user",(req,res)=>{
-    //saving data to the database
-    res.send("Data Successfully saved to the database!");
-});
-//this will match all the HTTP method API calls to /test
-app.use("/test",(req,res)=>{
-    res.send("hello from the server")
-});
-app.use((req, res)=> {
-    res.send("404 not found");
-});
+app.use("/user",
+    [
+        (req,res, next)=>{
+            //route handler
+            console.log("handling the route user1!!")
+            // res.send("Response1!!!") //response
+            next();
+        },
+
+        (req, res,next)=>{
+            console.log("Handling the route user 2");
+            next();
+        },
+    ],
+    (req, res,next)=>{
+            console.log("Handling the route user 3");
+            res.send("3rd response")
+            next();
+        },
+);
+   
 
 app.listen(3000,()=> {
     console.log("Server is successfully listening on port 3000")
